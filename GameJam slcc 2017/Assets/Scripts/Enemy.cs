94826e92e63419af533ddbd317855cef46cd6837
Spencer Isaacson <spencer_isaacson@yahoo.com>
2017-05-06 12:03:00 -0600

    Enemy View

---
+++ GameJam slcc 2017/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{ 
+	Patrolling,
+	Alerted,
+	Chasing
+}
+
+public class Enemy : MonoBehaviour {
+	
+
+	public State EnemyState = State.Patrolling;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (EnemyState);
+	}
+}
---
+++ GameJam slcc 2017/Assets/View.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		var enemy = transform.parent.gameObject.GetComponent<Enemy> ();
+		if (other.tag == "Player" && enemy.EnemyState == State.Patrolling)
+		{
+			RaycastHit hitInfo;
+			if (Physics.Raycast (transform.position, other.transform.position - transform.position, out hitInfo)) {
+				if (hitInfo.collider.tag == "Player")
+					transform.parent.gameObject.GetComponent<Enemy> ().EnemyState = State.Alerted;
+			}
+			
+		}
+	}
+}
