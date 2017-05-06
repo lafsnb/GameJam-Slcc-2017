0b31228e8cc6c746b0a7ad49dc8154b373427ced
Isaac <ikingst7@gmail.com>
2017-05-06 11:17:40 -0600

    Player Walks!!!

---
+++ GameJam slcc 2017/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Vector2 velocity;
+	Rigidbody rigidbody;
+	public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")) * speed;
+		rigidbody.velocity = velocity;
+	}
+}
