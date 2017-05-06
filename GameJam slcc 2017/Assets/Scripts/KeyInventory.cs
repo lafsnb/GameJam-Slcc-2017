11d0f1bfbb50a8ee08307bfb37dc2edcd8ead759
Isaac <ikingst7@gmail.com>
2017-05-06 15:07:17 -0600

    WIP unlocking doors

---
+++ GameJam slcc 2017/Assets/Scripts/KeyInventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInventory : MonoBehaviour {
+
+	public static List<DoorType> keys = new List<DoorType> ();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void AddKey(DoorType type) {
+		if (!keys.Contains (type)) {
+			keys.Add (type);
+			DoorManager.unlockDoors (keys);
+		}
+		print("key added");
+	}
+
+	public static void ResetKeys() {
+		keys.Clear ();
+	}
+}
