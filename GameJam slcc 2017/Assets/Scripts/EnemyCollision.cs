65d4adbcfb7616f95d4626840be6f9e2f78d1ad7
Spencer Isaacson <spencer_isaacson@yahoo.com>
2017-05-06 13:26:41 -0600

    Player Movement/Death

--- GameJam slcc 2017/Assets/Scripts/EnemyCollision.cs
+++ GameJam slcc 2017/Assets/Scripts/EnemyCollision.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class EnemyCollision : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Player") {
-			print ("Player Died");
+			SceneManager.LoadScene ("Game Over");
 		}
 	}
 }
