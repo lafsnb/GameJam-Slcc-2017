9766b331605b121894dee5241b25178fc0c7c30a
Spencer Isaacson <spencer_isaacson@yahoo.com>
2017-05-06 15:50:27 -0600

    Level Prefabs, first two levels

---
+++ GameJam slcc 2017/Assets/NextLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+	private static int level;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+			SceneManager.LoadScene (level++);
+	}
+}
--- GameJam slcc 2017/Assets/Scripts/LoadLevelFromFile.cs
+++ GameJam slcc 2017/Assets/Scripts/LoadLevelFromFile.cs
@@ -25,7 +25,6 @@ public class LoadLevelFromFile : MonoBehaviour {
 
 	void OnEnable()
 	{
-		Debug.Log("Fork");
 		Create ();
 	}
 	// Use this for initialization
