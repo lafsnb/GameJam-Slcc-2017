58e076badc833a98b6d019012107468389309553
Spencer Isaacson <spencer_isaacson@yahoo.com>
2017-05-07 15:11:02 -0600

    Title Screen, fixed respawn, end game returns to title screen

---
+++ GameJam slcc 2017/Assets/Scripts/Fader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Fader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	private float t;
+	void Update () {
+		GetComponent<Text> ().color = new Color (1, 1, 1, Mathf.Sin (t) / 2 + .5f);
+		t += Time.deltaTime;
+	}
+}
--- GameJam slcc 2017/Assets/Scripts/LoadWithDelay.cs
+++ GameJam slcc 2017/Assets/Scripts/LoadWithDelay.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadWithDelay : MonoBehaviour {
-
+	public float delay;
+	public string SceneName;
+	public bool loadLast;
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (Load());
 	}
 
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (loadLast)
+				LoadLast ();
+			else
+				SceneManager.LoadScene (SceneName);
+		}
+	}
 	IEnumerator Load()
 	{
-		yield return new WaitForSeconds (5f);
+		yield return new WaitForSeconds (delay);
+		if (loadLast)
+			LoadLast ();
+		else
+			SceneManager.LoadScene (SceneName);
+	}
+
+	void LoadLast()
+	{
 		if (LevelManagement.CurrentLevel != null)
 			SceneManager.LoadScene (LevelManagement.CurrentLevel);
 	}
