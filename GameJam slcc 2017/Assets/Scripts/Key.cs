4a55e9cad955ff2e616e2c87e802a826f6a50621
Spencer Isaacson <spencer_isaacson@yahoo.com>
2017-05-07 15:50:25 -0600

    Key HUD, level lighting

--- GameJam slcc 2017/Assets/Scripts/Key.cs
+++ GameJam slcc 2017/Assets/Scripts/Key.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Key : MonoBehaviour {
 
 	public DoorType type;
@@ -17,21 +17,30 @@ public class Key : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
+		var HUD = GameObject.Find ("Key Hud");
 		if (other.tag == "Player") {
             GameObject parentDoor = null;
             switch(type)
             {
-                case (DoorType.Blue):
-                    parentDoor = GameObject.Find("blueDoors");
+			case (DoorType.Blue):
+				parentDoor = GameObject.Find ("blueDoors");
+				if (HUD != null)
+					HUD.transform.Find ("Blue").GetComponent<Image> ().enabled = true;
                     break;
                 case (DoorType.Red):
                     parentDoor = GameObject.Find("redDoors");
+					if (HUD != null)
+						HUD.transform.Find ("Red").GetComponent<Image> ().enabled = true;
                     break;
                 case (DoorType.Yellow):
                     parentDoor = GameObject.Find("yellowDoors");
+					if (HUD != null)
+						HUD.transform.Find ("Yellow").GetComponent<Image> ().enabled = true;
                     break;
                 case (DoorType.Green):
                     parentDoor = GameObject.Find("greenDoors");
+					if (HUD != null)
+						HUD.transform.Find ("Green").GetComponent<Image> ().enabled = true;
                     break;
             }
             if (parentDoor != null)
